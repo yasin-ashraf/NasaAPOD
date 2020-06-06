3f64af9ad5d3487763bdb3c0e8bd6d7672745985
Yasin Ashraf <yasin.asharf@gmail.com>
2020-06-06 22:36:33 +0530

    first commit

---
+++ app/src/main/java/com/yasin/nasa/NasaActivity.kt
@@ -0,0 +1,12 @@
+package com.yasin.nasa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NasaActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
